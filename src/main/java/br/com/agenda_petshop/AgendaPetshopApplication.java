39f3651239f0f5474def43343ab55107fea5dcd2
Mateus Barbosa <mateuscbarbosa@gmail.com>
2024-07-30 09:35:43 -0300

    first commit

---
+++ src/main/java/br/com/agenda_petshop/AgendaPetshopApplication.java
@@ -0,0 +1,13 @@
+package br.com.agenda_petshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaPetshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaPetshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/agenda_petshop/AgendaPetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agenda_petshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaPetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
